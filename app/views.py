ee4d074d2383569019e75066c9e8b7ed21c35062
unohayato <mamidori1p1p@gmail.com>
2022-12-11 22:07:50 +0900

    done

--- app/views.py
+++ app/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
+from .models import Post
 # Create your views here.
 
-def index(request):
-  return render(request, 'app/index.html')
+class Index(ListView):
+  model = Post
+  
+class Detail(DetailView):
+  model = Post
+  
+class Create(CreateView):
+  model = Post
+  fields = ['title', 'body', 'category', 'tags']
+  
+  success_url = "/"
+
+class Update(UpdateView):
+  model = Post
+  fields = ["title", "body", "category", "tags"]
+  
+  success_url = "/"
+
+class Delete(DeleteView):
+  model = Post
+  
+  success_url = "/"
+  
+  
+  
+  
