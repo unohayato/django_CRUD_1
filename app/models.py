67ec4379ed6ec3e95ce54f670c58511c2be9dad1
unohayato <mamidori1p1p@gmail.com>
2022-12-15 23:24:35 +0900

    models

---
+++ app/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Category(models.Model):
+  name = models.CharField(max_length=255, blank=False, null=False, unique=True)
+  
+  def __str__(self):
+    return self.name
+  
+class Tag(models.Model):
+  name = models.CharField(max_length=255, blank=False, null=False, unique=True)
+  
+  def __str__(self):
+    return self.name
+
+class Post(models.Model):
+  title = models.CharField(max_length=255, blank=False, null=False)
+  body = models.TextField(blank=True, null=False)
+  created = models.DateTimeField(auto_now_add=True, editable=False, blank=False, null=False)
+  updated = models.DateTimeField(auto_now=True, editable=False, blank=False, null=False)
+  category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False)
+  tags = models.ManyToManyField(Tag, blank=True)
+  
+  def __str__(self):
+    return self.name
+  
